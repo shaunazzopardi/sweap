24a604aa82faf3847aa379f01e2c663d7c563dfa
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2023-09-21 12:51:53 +0200

    better fault localisation
    when concretising counterexample, and there is a state mismatch now identifying when in reality this a transition mismatch

--- src/analysis/abstraction/concretisation.py
+++ src/analysis/abstraction/concretisation.py
@@ -2,7 +2,8 @@ from pysmt.shortcuts import And
 
 from analysis.abstraction.interface.predicate_abstraction import PredicateAbstraction
 from analysis.smt_checker import SMTChecker
-from programs.util import looping_to_normal, stutter_transition, preds_in_state, var_to_predicate, is_predicate_var
+from programs.util import looping_to_normal, stutter_transition, preds_in_state, var_to_predicate, is_predicate_var, \
+    transition_formula, add_prev_suffix
 from prop_lang.biop import BiOp
 from prop_lang.util import neg, conjunct_formula_set, disjunct_formula_set, propagate_negations, sat, \
     simplify_formula_with_math
@@ -35,70 +36,89 @@ def concretize_transitions(program,
     # two options, either we stopped because of a predicate mismatch, or a transition mismatch
     incompatibility_formula = []
     if incompatible_state["compatible_states"] == "FALSE" or incompatible_state["compatible_outputs"] == "FALSE":
-        if program.deterministic:
-            failed_condition = neg(concretized[-1][0].condition)
-            reduced = failed_condition.replace([BiOp(Variable(str(v)), ":=", Value(concretized[-1][1][str(v)]))
-                                                for v in program.env_events + program.con_events])
-            reduced_simplified = simplify_formula_with_math(reduced, program.symbol_table)
-
-            return concretized[:-1], ([reduced_simplified], concretized[-1][1]), concretized[-1]
-        else:
-            # if program is not deterministic, we need to identify the transitions the counterstrategy wanted to take rather than the one the program actually took
-            try:
-                state_before_mismatch = concretized[-2][1]
-            except Exception as e:
-                raise e
-            src_state = concretized[-2][0].tgt
-            tgt_state_env_wanted = [p for p in program.states if incompatible_state["mon_" + str(p)] == "TRUE"][0]
-            outputs_env_wanted = [p for p in program.out_events if incompatible_state["mon_" + str(p)] == "TRUE"]
-            outputs_env_wanted += [neg(p) for p in program.out_events if incompatible_state["mon_" + str(p)] == "FALSE"]
-            if incompatible_state["turn"] == "mon_env":
-                candidate_transitions = [t for t in program.env_transitions if
-                                         t.src == src_state and t.tgt == tgt_state_env_wanted and set(t.output) == set(
-                                             outputs_env_wanted)]
-                if tgt_state_env_wanted == src_state:
-                    stutter = stutter_transition(program, src_state, True)
-                    if stutter is not None:
-                        candidate_transitions.append(stutter)
-            elif incompatible_state["turn"] == "mon_con":
-                candidate_transitions = [t for t in program.con_transitions if
-                                         t.src == src_state and t.tgt == tgt_state_env_wanted and set(t.output) == set(
-                                             outputs_env_wanted)]
-                if tgt_state_env_wanted == src_state:
-                    stutter = stutter_transition(program, src_state, False)
-                    if stutter is not None:
-                        candidate_transitions.append(stutter)
-            else:
-                raise Exception("Mismatches should only occur at mon_env or mon_con turns")
-
-            compatible_with_abstract_state = []
-            for str_p in state_before_mismatch.keys():
-                if is_predicate_var(str_p):
-                    p = Variable(str_p)
-                    if state_before_mismatch[str_p] == "TRUE":
-                        compatible_with_abstract_state += [var_to_predicate(p)]
-                    else:
-                        compatible_with_abstract_state += [neg(var_to_predicate(p))]
-
-            abstract_state = conjunct_formula_set(compatible_with_abstract_state)
-            env_desired_transitions = [t for t in candidate_transitions
-                                       if smt_checker.check(And(*abstract_state.to_smt(program.symbol_table),
-                                                                *t.condition.to_smt(program.symbol_table)))]
-            formula = disjunct_formula_set([t.condition for t in env_desired_transitions] + [
-                propagate_negations(neg(concretized[-1][0].condition))])
-            return concretized[:-1], ([formula], concretized[-1][1]), concretized[-1]
+        return process_transition_mismatch(program,
+                                           concretized,
+                                           incompatible_state,
+                                           smt_checker)
     else:
-        env_pred_state = None
         if incompatible_state["compatible_state_predicates"] == "FALSE" or incompatible_state[
             "compatible_tran_predicates"] == "FALSE":
-            # pred mismatch
             pred_state = preds_in_state(incompatible_state)
-            for p in pred_state:
-                if not sat(conjunct_formula_set([p] + [BiOp(v, "=", Value(incompatible_state[str(v)])) for v in p.variablesin()]),
-                       program.symbol_table):
-                    incompatibility_formula.append(p)
+            predicate_state_before_incompatibility = list(map(add_prev_suffix, preds_in_state(concretized[-2][1])))
 
-            env_pred_state = (incompatibility_formula, incompatible_state)
+            # we check if this incompatible state formula is ever possibly true after the last transition
+                # if it is then the problem is with the predicate state
+            if sat(conjunct_formula_set(pred_state + predicate_state_before_incompatibility + [transition_formula(concretized[-1][0])]), program.symbol_table):
+                # reduce predicate mismatch to the actually mismatched predicates
+                for p in pred_state:
+                    var_state = [BiOp(v, "=", Value(incompatible_state[str(v)])) for v in p.variablesin()]
+                    if not sat(conjunct_formula_set([p] + var_state),
+                           program.symbol_table):
+                        incompatibility_formula.append(p)
 
-        return concretized, env_pred_state, concretized[-1]
+                env_pred_state = (incompatibility_formula, incompatible_state)
+                return concretized, env_pred_state, concretized[-1]
+            # if not, then we choose the wrong transition
+            else:
+                return process_transition_mismatch(program,
+                                                    concretized,
+                                                    incompatible_state,
+                                                    smt_checker)
 
+
+def process_transition_mismatch(program,
+                                concretized,
+                                incompatible_state,
+                                smt_checker):
+    if program.deterministic:
+        failed_condition = neg(concretized[-1][0].condition)
+        reduced = failed_condition.replace([BiOp(Variable(str(v)), ":=", Value(concretized[-1][1][str(v)]))
+                                            for v in program.env_events + program.con_events])
+        reduced_simplified = simplify_formula_with_math(reduced, program.symbol_table)
+
+        return concretized[:-1], ([reduced_simplified], concretized[-1][1]), concretized[-1]
+    else:
+        # if program is not deterministic, we need to identify the transitions the counterstrategy wanted to take rather than the one the program actually took
+        try:
+            state_before_mismatch = concretized[-2][1]
+        except Exception as e:
+            raise e
+        src_state = concretized[-2][0].tgt
+        tgt_state_env_wanted = [p for p in program.states if incompatible_state["mon_" + str(p)] == "TRUE"][0]
+        outputs_env_wanted = [p for p in program.out_events if incompatible_state["mon_" + str(p)] == "TRUE"]
+        outputs_env_wanted += [neg(p) for p in program.out_events if incompatible_state["mon_" + str(p)] == "FALSE"]
+        if incompatible_state["turn"] == "mon_env":
+            candidate_transitions = [t for t in program.env_transitions if
+                                     t.src == src_state and t.tgt == tgt_state_env_wanted and set(t.output) == set(
+                                         outputs_env_wanted)]
+            if tgt_state_env_wanted == src_state:
+                stutter = stutter_transition(program, src_state, True)
+                if stutter is not None:
+                    candidate_transitions.append(stutter)
+        elif incompatible_state["turn"] == "mon_con":
+            candidate_transitions = [t for t in program.con_transitions if
+                                     t.src == src_state and t.tgt == tgt_state_env_wanted and set(t.output) == set(
+                                         outputs_env_wanted)]
+            if tgt_state_env_wanted == src_state:
+                stutter = stutter_transition(program, src_state, False)
+                if stutter is not None:
+                    candidate_transitions.append(stutter)
+        else:
+            raise Exception("Mismatches should only occur at mon_env or mon_con turns")
+
+        compatible_with_abstract_state = []
+        for str_p in state_before_mismatch.keys():
+            if is_predicate_var(str_p):
+                p = Variable(str_p)
+                if state_before_mismatch[str_p] == "TRUE":
+                    compatible_with_abstract_state += [var_to_predicate(p)]
+                else:
+                    compatible_with_abstract_state += [neg(var_to_predicate(p))]
+
+        abstract_state = conjunct_formula_set(compatible_with_abstract_state)
+        env_desired_transitions = [t for t in candidate_transitions
+                                   if smt_checker.check(And(*abstract_state.to_smt(program.symbol_table),
+                                                            *t.condition.to_smt(program.symbol_table)))]
+        formula = disjunct_formula_set([t.condition for t in env_desired_transitions] + [
+            propagate_negations(neg(concretized[-1][0].condition))])
+        return concretized[:-1], ([formula], concretized[-1][1]), concretized[-1]
