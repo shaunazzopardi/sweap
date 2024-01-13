d2febd54461e6c5aa28927f5668fce4be9220d7f
Luca Di Stefano <lucadistefano90@gmail.com>
2024-01-13 18:58:35 +0100

    ltlmt2prog: do not preprocess Program

--- src/parsing/string_to_ltlmt.py
+++ src/parsing/string_to_ltlmt.py
@@ -242,7 +242,7 @@ class ToProgram(NodeWalker):
 
         prog = Program(
             name, ['e0', 'c0'], 'e0', init_values, env_t, con_t,
-            list(self.env_events), con_events, [])
+            list(self.env_events), con_events, [], preprocess=False)
 
         formula = normalize_ltl(orig_formula)
 
