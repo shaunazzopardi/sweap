a71887f1829a953067b6b3d45398526492615372
Luca Di Stefano <lucad@chalmers.se>
2024-04-10 12:41:33 +0000

    move setup.sh to scripts/

---
+++ scripts/setup.sh
@@ -0,0 +1,5 @@
+hon3-pip docker libgmp-dev swig
+pip3 install z3-solver
+pip3 install -r requirements.txt
+pysmt-install --msat
+pysmt-install --bdd
--- setup.sh
+++
@@ -1,5 +0,0 @@
-# Dependencies for run.sh and the binaries
-# TODO turn this into a Dockerfile
-
-apt install z3 python3-pip
-pip3 install z3-solver
