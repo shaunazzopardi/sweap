b735efbb8526c276256cd361d06fd113e85d3afc
Luca Di Stefano <lucad@chalmers.se>
2024-04-11 08:31:16 +0000

    Add run-raboniel.sh

---
+++ scripts/run-raboniel.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Parts of this script were adapted from a
+# software artifact available at:
+# https://zenodo.org/doi/10.5281/zenodo.8409938
+
+TIMEOUT=600
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BASEPATH=$SCRIPT_DIR/../binaries
+TIMESTAMP=`date +%Y-%m-%d-%H-%M-%S`
+OUTFILE=$SCRIPT_DIR/out-raboniel-$TIMESTAMP
+LOGFILE=$SCRIPT_DIR/log-raboniel-$TIMESTAMP
+BENCHMARKS_DIR=$SCRIPT_DIR/../examples/benchmarks/raboniel
+
+run_raboniel() {
+	echo "Run raboniel on $1 at $(date +%H:%M:%S)" 
+	echo "Run raboniel on $1 at $(date +%H:%M:%S)" >> $OUTFILE
+	echo "Benchmark: $1" >> $OUTFILE
+	echo "Benchmark: $1" >> $LOGFILE
+	s=`date +%s%N`
+	timeout $TIMEOUT ./raboniel --spec $1 2>> $LOGFILE | grep 'Result' >> $OUTFILE
+	e=`date +%s%N` 
+	echo "Runtime: $(((e - s)/1000000))ms" >> $OUTFILE
+	echo "" >> $OUTFILE
+	echo "" >> $LOGFILE
+}
+
+
+oldpath=$PWD
+cd $BASEPATH
+for f in `ls $BENCHMARKS_DIR/*.tslmt`
+do
+  run_raboniel $f
+done
+cd $oldpath
