13ff37d1a2c4141471ca23c31ab44c76a03c098a
Luca Di Stefano <lucad@chalmers.se>
2024-04-11 08:29:59 +0000

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,6 @@
-hon3-pip docker libgmp-dev swig
+hon3-pip docker libgmp-dev swig openjdk-17-jre
 pip3 install z3-solver
 pip3 install -r requirements.txt
 pysmt-install --msat
 pysmt-install --bdd
+
