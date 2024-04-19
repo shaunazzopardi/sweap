88fb61b5b5ae748210174d07ff44ceffed0f2dc8
Luca Di Stefano <lucad@chalmers.se>
2024-04-19 14:23:43 +0000

    Fix sweap/sweap-noacc scripts

--- scripts/run-sweap.sh
+++ scripts/run-sweap.sh
@@ -9,7 +9,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BASEPATH=$SCRIPT_DIR/..
 TIMESTAMP=`date +%Y-%m-%d-%H-%M-%S`
 OUTFILE=$SCRIPT_DIR/out-sweap-$TIMESTAMP
-LOGFILE=$SCRIPT_DIR/log-sweap-$TIMESTAMP
 BENCHMARKS_DIR=$SCRIPT_DIR/../examples/benchmarks/sweap
 echo "run-sweap.sh" >> $OUTFILE
 echo "" >> $OUTFILE
@@ -19,7 +18,7 @@ run_sweap() {
   echo "Run Sweap on $1 at $(date +%H:%M:%S)"
   echo "Benchmark: $1" >> $OUTFILE
   s=`date +%s%N`
-  PATH=$BASEPATH/binaries/CPAchecker-2.3-unix/scripts:$BASEPATH/binaries:$PATH PYTHONPATH=$BASEPATH/src/ timeout $TIMEOUT python3 $BASEPATH/main.py --accelerate --synthesise  --p $1  2> $LOGFILE > $tmpout
+  PATH=$BASEPATH/binaries/CPAchecker-2.3-unix/scripts:$BASEPATH/binaries:$PATH PYTHONPATH=$BASEPATH/src/ timeout $TIMEOUT python3 $BASEPATH/main.py --accelerate --synthesise  --p $1 > $tmpout
   grep "^\(Unr\|R\)ealizable\." $tmpout >> $OUTFILE
   e=`date +%s%N`
   echo "Runtime: $(((e - s)/1000000))ms" >> $OUTFILE
