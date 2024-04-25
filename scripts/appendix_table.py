068e5bcd037ab90ec90c89edbe0913f9ef593122
Luca Di Stefano <lucad@chalmers.se>
2024-04-25 12:58:11 +0000

    Fix appendix_table.py

--- scripts/appendix_table.py
+++ scripts/appendix_table.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 
 writer = csv.writer(sys.stdout)
-prefix = re.compile(r"out-sweap(-noacc)?-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-")
+prefix = re.compile(r"out-sweap(-noacc)?-+")
 
 def process(fname, row_id):
     def shell(cmd):
@@ -16,8 +16,9 @@ def process(fname, row_id):
     def handle_predicates_line(line):
         preds = (
             line.
+            strip().
             replace("adding ", "").
-            replace(" to predicate abstraction\n", "").
+            replace(" to predicate abstraction", "").
             split(", "))
         preds = [p for p in preds if p]
         tr = sum(1 for p in preds if "_prev" in p)
@@ -28,41 +29,42 @@ def process(fname, row_id):
         f"""grep -A1 "Starting abstract synthesis loop." {fname} | tail -n1""")
     init_st, init_tr = handle_predicates_line(init_preds)
 
+    bench_name = Path(fname).stem.partition('.')[0]
+    bench_name = re.sub(prefix, "", bench_name)
+
     all_pred_lines = shell(f"""grep "^adding" {fname}""").split("\n")
-    all_pred_lines = (handle_predicates_line(x) for x in all_pred_lines if x)
-    all_st, all_tr = zip(*all_pred_lines)
-    add_st, add_tr = sum(all_st) - init_st, sum(all_tr) - init_tr
+    all_pred_lines = [handle_predicates_line(x) for x in all_pred_lines if x]
+    add_st, add_tr = 0, 0
+    if len(all_pred_lines):
+        all_st, all_tr = zip(*all_pred_lines)
+        add_st, add_tr = sum(all_st) - init_st, sum(all_tr) - init_tr
 
     count_fair_ref = shell(
         f"""grep "Structural Refinement" {fname} | wc -l""").strip()
     count_safe_ref = shell(
         f"""grep "safety refinement" {fname} | wc -l""").strip()
     
-    bench_name = Path(fname).stem.partition('.')[0]
-    bench_name = re.sub(prefix, "", bench_name)
+    
 
     writer.writerow([
         row_id, bench_name,
         init_st, init_tr, count_fair_ref, count_safe_ref, add_st, add_tr])
 
-header = [
-    "row-id",
-    "bench name (sweap_acc)", "init st", "init tr", 
-    "liveness refinements", "safety refinements",
-    "added st", "added tr"]
 
-logs_sweap = [Path(x).resolve() for x in glob.glob("logs/out-sweap-2*.log")]
-logs_sweap_noacc = [Path(x).resolve() for x in glob.glob("logs/out-sweap-noacc*.log")]
-writer.writerow(header)
-for i, log in enumerate(sorted(logs_sweap), start=2):
-    process(log, i)
-print("-"*30)
-header[1] = "bench name (sweap_no-acc)"
-writer.writerow(header)
-for i, log in enumerate(sorted(logs_sweap_noacc), start=2):
-    process(log, i)
+if __name__ == "__main__":
+    header = [
+        "row-id",
+        "bench name (sweap_acc)", "init st", "init tr", 
+        "liveness refinements", "safety refinements",
+        "added st", "added tr"]
 
-# process("/home/luca/git/sweap/scripts/logs/out-sweap-2024-04-23-15-15-41-arbiter-paper-5.prog.log")
-# process("/home/luca/git/sweap/scripts/logs/out-sweap-noacc-2024-04-23-15-17-18-robot-grid-reach-2d-5.prog.log")
-
-# print()
+    logs_sweap = [Path(x).resolve() for x in glob.glob("logs/out-sweap--*.log")]
+    logs_sweap_noacc = [Path(x).resolve() for x in glob.glob("logs/out-sweap-noacc*.log")]
+    writer.writerow(header)
+    for i, log in enumerate(sorted(logs_sweap), start=2):
+        process(log, i)
+    print("-"*30)
+    header[1] = "bench name (sweap_no-acc)"
+    writer.writerow(header)
+    for i, log in enumerate(sorted(logs_sweap_noacc), start=2):
+        process(log, i)
