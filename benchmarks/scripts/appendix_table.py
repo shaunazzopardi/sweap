9c65b120ca9e5a2c1b3b2f73864eb1afda192514
luca <luca.di.stefano@tuwien.ac.at>
2024-10-04 12:20:30 +0000

    Update appendix_table.py

--- benchmarks/scripts/appendix_table.py
+++ benchmarks/scripts/appendix_table.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 import csv
 import glob
 import sys
@@ -10,7 +10,27 @@ from pathlib import Path
 writer = csv.writer(sys.stdout)
 prefix = re.compile(r"out-sweap(-noacc)?-+")
 
+skip = (
+    "solitary", "infinite-race", "square5x5", "chain-simple-param-70",
+    "robot-collect-samples-v4",
+    *(f"{b}-paper-{n}" for n in (5, 10, 50) for b in ("arbiter", "elevator")),
+    *(f"arbiter-paper-unreal-{n}" for n in (5, 10, 50)),
+    *(f"bloem-elevator-{t}-{n}" for n in (3, 5, 10, 50) for t in ("simple", "signal")),
+    *(f"robot-grid-reach-{d}d-{n}" for n in (3, 5, 10, 50) for d in (1, 2)),
+    *(f"reversible-lane-{r}-{n}" for n in (3, 5, 10, 50) for r in ("r", "u")),
+)
+rename = {
+    "robot-grid-reach-repeated-with-obstacles-1d": "rep-reach-obst.-1d",
+    "robot-grid-reach-repeated-with-obstacles-2d": "rep-reach-obst.-2d"
+}
+
 def process(fname, row_id):
+    bench_name = Path(fname).stem.partition('.')[0]
+    bench_name = re.sub(prefix, "", bench_name)
+    bench_name = bench_name.replace("_", "-")
+    bench_name = rename.get(bench_name, bench_name)
+    if bench_name in skip:
+        return False
     def shell(cmd):
         return check_output(cmd, shell=True, encoding="utf-8")
     def handle_predicates_line(line):
@@ -29,26 +49,28 @@ def process(fname, row_id):
         f"""grep -A1 "Starting abstract synthesis loop." {fname} | tail -n1""")
     init_st, init_tr = handle_predicates_line(init_preds)
 
-    bench_name = Path(fname).stem.partition('.')[0]
-    bench_name = re.sub(prefix, "", bench_name)
 
-    all_pred_lines = shell(f"""grep "^adding" {fname}""").split("\n")
-    all_pred_lines = [handle_predicates_line(x) for x in all_pred_lines if x]
+    try:
+        all_pred_lines = shell(f"""grep "^adding" {fname}""").split("\n")
+        all_pred_lines = [handle_predicates_line(x) for x in all_pred_lines if x]
+    except CalledProcessError:
+        all_pred_lines = []
     add_st, add_tr = 0, 0
+    count_fair_ref, count_safe_ref = 0, 0
     if len(all_pred_lines):
         all_st, all_tr = zip(*all_pred_lines)
         add_st, add_tr = sum(all_st) - init_st, sum(all_tr) - init_tr
 
-    count_fair_ref = shell(
-        f"""grep "Structural Refinement" {fname} | wc -l""").strip()
-    count_safe_ref = shell(
-        f"""grep "safety refinement" {fname} | wc -l""").strip()
-    
+        count_fair_ref = shell(
+            f"""grep "Structural Refinement" {fname} | wc -l""").strip()
+        count_safe_ref = shell(
+            f"""grep "safety refinement" {fname} | wc -l""").strip()
     
 
     writer.writerow([
         row_id, bench_name,
         init_st, init_tr, count_fair_ref, count_safe_ref, add_st, add_tr])
+    return True
 
 
 if __name__ == "__main__":
@@ -58,13 +80,20 @@ if __name__ == "__main__":
         "liveness refinements", "safety refinements",
         "added st", "added tr"]
 
-    logs_sweap = [Path(x).resolve() for x in glob.glob("logs/out-sweap--*.log")]
-    logs_sweap_noacc = [Path(x).resolve() for x in glob.glob("logs/out-sweap-noacc*.log")]
+    logs_dir = Path(sys.argv[1])
+    logs_sweap = [Path(x).resolve() for x in logs_dir.glob("out-sweap--*.log")]
+    logs_sweap_noacc = [Path(x).resolve() for x in logs_dir.glob("out-sweap-noacc*.log")]
     writer.writerow(header)
-    for i, log in enumerate(sorted(logs_sweap), start=2):
-        process(log, i)
+    i = 2
+    for log in sorted(logs_sweap_noacc):
+        ok = process(log, i)
+        if ok:
+            i += 1
     print("-"*30)
     header[1] = "bench name (sweap_no-acc)"
     writer.writerow(header)
-    for i, log in enumerate(sorted(logs_sweap_noacc), start=2):
-        process(log, i)
+    i = 2
+    for log in sorted(logs_sweap_noacc):
+        ok = process(log, i)
+        if ok:
+            i += 1
