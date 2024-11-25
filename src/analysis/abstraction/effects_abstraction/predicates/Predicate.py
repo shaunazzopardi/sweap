75dad7e9a2d01358979757cca15d7dd3d7073fd4
shaun <shaun.azzopardi@gmail.com>
2024-11-25 15:17:51 +0100

    Predicate superclass, with refine_nexts static function

---
+++ src/analysis/abstraction/effects_abstraction/predicates/Predicate.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+
+from prop_lang.formula import Formula
+from prop_lang.util import conjunct, sat
+from prop_lang.variable import Variable
+
+
+class Predicate(ABC):
+
+    @abstractmethod
+    def variablesin(self):
+        pass
+
+    @abstractmethod
+    def boolean_rep(self):
+        pass
+
+    @abstractmethod
+    def extend_effect_now(self,
+                      gu: Formula,
+                      effect: [(Formula, dict[Variable, [Formula]])],
+                      symbol_table) -> [(Formula, dict[Variable, [Formula]])]:
+        pass
+
+    @abstractmethod
+    def extend_effect_next(self,
+                      gu: Formula,
+                      effect: [(Formula, dict[Variable, [Formula]])],
+                      symbol_table) -> [(Formula, dict[Variable, [Formula]])]:
+        pass
+
+    @abstractmethod
+    def extend_effect(self,
+                      gu: Formula,
+                      effect: [(Formula, dict[Variable, [Formula]])],
+                      symbol_table) -> [(Formula, dict[Variable, [Formula]])]:
+        pass
+
+    @abstractmethod
+    def is_pre_cond(self, gu: Formula, symbol_table):
+        pass
+
+    @abstractmethod
+    def is_invar(self, gu: Formula, symbol_table):
+        pass
+
+    @abstractmethod
+    def is_post_cond(self, gu: Formula, symbol_table):
+        pass
+
+def refine_nexts(now, nexts, symbol_table):
+    new_nexts = []
+    for v_next in nexts:
+        if sat(conjunct(now, v_next), symbol_table):
+            new_nexts.append(v_next)
+    return new_nexts
