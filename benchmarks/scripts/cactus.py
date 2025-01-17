ca6a844564d76993b4cbba634cff7549da2648f6
luca <luca.di.stefano@tuwien.ac.at>
2025-01-17 12:41:57 +0000

    Update cactus.py

--- benchmarks/scripts/cactus.py
+++ benchmarks/scripts/cactus.py
@@ -5,9 +5,12 @@ import matplotlib as mpl
 from itertools import cycle
 
 TIMEOUT = 1200000
-FILENAME = "../../newnew.csv"
-# FILENAME = "infinitestate-stela.csv"
-# sns.set_theme("notebook")
+FILENAME, is_old_dataset = "../../newnew.csv", False
+# FILENAME, is_old_dataset = "infinitestate-stela.csv", True
+exclude_full_ltl = False
+lineplot_config = dict(
+    markers="osdPso^X", markersize=6
+)
 
 
 tools = [
@@ -26,6 +29,30 @@ legend = {
     "tslmt2rpg": "tslmt2rpg (realisability)"
 }
 
+linestyles = {
+    legend["sweap"]: "-",
+    legend["sweap-noacc"]: "--",
+    legend["raboniel"]: "-.",
+    legend["temos"]: "--",
+    legend["rpgsolve-syn"]: "-",
+    legend["rpgsolve"]: ":",
+    legend["rpg-stela"]: ":",
+    legend["tslmt2rpg"]: ":"
+}
+
+full_ltl_benchs = (
+    "arbiter",
+    "arbiter-with-failure",
+    "elevator",
+    "reversible-lane-r",
+    "reversible-lane-u",
+    "robot_collect_samples_v4",
+    "robot-grid-reach-repeated-with-obstacles-1d",
+    "robot-grid-reach-repeated-with-obstacles-2d",
+    "taxi-service"
+) if exclude_full_ltl else tuple()
+
+
 def cum_sum(tool_name: str):
     # col_name = tool_name
     col_name = legend.get(tool_name, tool_name)
@@ -33,6 +60,7 @@ def cum_sum(tool_name: str):
         pl.scan_csv(FILENAME)
         .filter(pl.col(tool_name) > 2)
         .filter(pl.col(tool_name) < TIMEOUT)
+        .filter(pl.col("benchmark").is_in(full_ltl_benchs).not_())
         .sort(by=tool_name)
         .with_row_index("instances", 1)
         .select(instances=pl.col("instances"), **{col_name: pl.cum_sum(tool_name)/1000})
@@ -42,14 +70,6 @@ def cum_sum(tool_name: str):
         pl.DataFrame({"instances": [0], col_name: [None]})
         .cast({"instances": pl.UInt32, col_name: pl.Float64}))
     return pl.concat([zero, df])
-    # return df
-
-# def remove_right(df: pl.DataFrame):
-#     return {
-#         x: x[:-6]
-#         for x in df.columns
-#         if x[-6:] == "_right"}
-
 
 dataframes = [cum_sum(x) for x in tools]
 joined = dataframes[0]
@@ -68,15 +88,18 @@ print(joined)
 easy_df = joined.limit(20)
 plot_easy = sns.lineplot(
     data=easy_df.drop("instances").to_pandas(),
-    markers=True, markeredgecolor=None, markeredgewidth=1, markersize=5)
-
-# for ln, style in zip(plot_easy.lines, [x["line"] for x in style.values()]):
-#     ln.set_linestyle(style)
-# plot_easy.legend()
+    **lineplot_config)
 
 # Some black magic to sort & add headers to our legend
 dummy = mpl.lines.Line2D([], [], color="none")
 handles, labels = plot_easy.get_legend_handles_labels()
+
+# Force linestyles
+for i, tool in enumerate(easy_df.drop("instances").columns):
+    for ln in (plot_easy.lines[i], handles[i]):
+        ln.set_linestyle(linestyles.get(tool, '-'))
+        ln.set_linewidth(1.2)
+
 zipped = list(zip(handles, labels))
 
 # Save styles for later
@@ -98,11 +121,7 @@ labels.insert(6, "$\\bf{Realisability}$")
 
 plot_easy.legend(handles, labels, ncol=2)
 
-
-
-
-
-plot_easy.set_title("Behaviour on first 20 solved instances")
+plot_easy.set_title(f"Behaviour on first 20 solved instances{' (excl. novel LTL instances)' if exclude_full_ltl else ''}")
 plot_easy.set(xlabel="Instances solved")
 plot_easy.set(ylabel="Time (s)")
 plot_easy.set(yscale='log')
@@ -111,7 +130,7 @@ plot_easy.set(xticks=[1,5,10,15,20])
 
 fig = plot_easy.get_figure()
 fig.tight_layout()
-fig.savefig("cactus-easy.png", dpi=300)
+fig.savefig("cactus-easy.pdf", dpi=300)
 
 fig.clear()
 
@@ -122,27 +141,17 @@ real_df = (
 
 plot_real = sns.lineplot(
     data=real_df.drop("instances").to_pandas(),
-    markers=True, markeredgecolor=None, markeredgewidth=1, markersize=5)
+    **lineplot_config)
 
 # Keep line styles consistent
 handles, labels = plot_real.get_legend_handles_labels()
 
-linestyles = {
-    legend["sweap"]: "-",
-    legend["sweap-noacc"]: "--",
-    legend["raboniel"]: ":",
-    legend["temos"]: "-.",
-    legend["rpgsolve"]: "--",
-    legend["rpgsolve-syn"]: "-",
-    legend["rpg-stela"]: "-.",
-    legend["tslmt2rpg"]: "-."
-}
 for i, tool in enumerate(real_df.drop("instances").columns):
     for ln in (plot_real.lines[i], handles[i]):
         ln.set_color(styles[tool].get_color())
-        ln.set_linestyle(linestyles[tool])
+        ln.set_linewidth(styles[tool].get_linewidth())
+        ln.set_linestyle(linestyles.get(tool, '-'))
         ln.set_marker(styles[tool].get_marker())
-# labels = [x.replace(" (realisability)", "") for x in labels]
 
 # Sort legend alphabetically
 zipped = sorted(zip(handles, labels), key=lambda x: x[1])
@@ -152,14 +161,14 @@ plot_real.legend(handles, labels)
 
 
 
-plot_real.set_title("Realisability")
+plot_real.set_title(f"Realisability{' (excl. novel LTL instances)' if exclude_full_ltl else ''}")
 plot_real.set(xlabel="Instances solved")
 plot_real.set(ylabel="Time (s)")
 
 
 fig = plot_real.get_figure()
 fig.tight_layout()
-fig.savefig("cactus-real.png", dpi=300)
+fig.savefig("cactus-real.pdf", dpi=300)
 
 ## Synthesis results
 fig.clear()
@@ -168,10 +177,8 @@ syn_df = (
     joined
     .drop(*(name for name in legend.values() if "(synthesis)" not in name)))
 
-plot_syn = sns.lineplot(
-    data=syn_df.drop("instances").to_pandas(),
-    markers=True, markeredgecolor=None, markeredgewidth=1, markersize=5)
-plot_syn.set_title("Synthesis")
+plot_syn = sns.lineplot(data=syn_df.drop("instances").to_pandas(), **lineplot_config)
+plot_syn.set_title(f"Synthesis{' (excl. novel LTL instances)' if exclude_full_ltl else ''}")
 plot_syn.set(xlabel="Instances solved")
 plot_syn.set(ylabel="Time (s)")
 
@@ -181,8 +188,8 @@ handles, labels = plot_syn.get_legend_handles_labels()
 for i, tool in enumerate(syn_df.drop("instances").columns):
     for ln in (plot_syn.lines[i], handles[i]):
         ln.set_color(styles[tool].get_color())
-        ln.set_linestyle(linestyles[tool])
-        # ln.set_linestyle(styles[tool].get_linestyle())
+        ln.set_linewidth(styles[tool].get_linewidth())
+        ln.set_linestyle(linestyles.get(tool, '-'))
         ln.set_marker(styles[tool].get_marker())
 labels = [x.replace(" (realisability)", "").replace(" (synthesis)", "") for x in labels]
 # Sort legend alphabetically
@@ -192,7 +199,7 @@ plot_syn.legend(handles, labels)
 
 fig = plot_syn.get_figure()
 fig.tight_layout()
-fig.savefig("cactus-syn.png", dpi=300)
+fig.savefig("cactus-syn.pdf", dpi=300)
 
 
 
@@ -200,4 +207,4 @@ fig.savefig("cactus-syn.png", dpi=300)
 
 # fig = plot.get_figure()
 # fig.tight_layout()
-# fig.savefig("cactus-log.png", dpi=300)
+# fig.savefig("cactus-log.pdf", dpi=300)
