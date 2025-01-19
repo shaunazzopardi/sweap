10c229d1aa541c723430224354fc1c9ca53291e7
luca <luca.di.stefano@tuwien.ac.at>
2025-01-19 13:32:18 +0000

    Add tslmt2rpg synthesis script

---
+++ binaries/run-pruned-syn.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Parts of this script were adapted from a
+# software artifact available at:
+# https://doi.org/10.5281/zenodo.13939202
+
+INPUT=$1
+echo "Eval tslmt2rpg [PRUNED] on $INPUT at $(date +%H:%M:%S)"
+echo "Run tslmt2rpg at $(date +%H:%M:%S)"
+game=$(tslmt2rpg --prune --rules-disable-precise-deduction --muval-caller call-muval.sh  --muval-timeout 20 < $INPUT) 
+echo "Run rpgsolve at $(date +%H:%M:%S)"
+res=$(echo "$game" | rpgsolve --disable-log --generate-program)
+echo "$res"
