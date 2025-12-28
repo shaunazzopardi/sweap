63f9abe886cc51f6f65b8e5455991afde18d07bf
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2025-12-28 21:00:53 +0100

    feat: upgraded interpolation_refinement to handle inputs
    - when no interpolant found, QE is used to learn new predicates for each step, stopping at the first step new preds are found

--- src/analysis/refinement/safety_refinement/interpolation_refinement.py
+++ src/analysis/refinement/safety_refinement/interpolation_refinement.py
@@ -1,15 +1,17 @@
 import logging
 
+from pysmt.shortcuts import And, ForAll, Implies
+
 from analysis.abstraction.effects_abstraction.effects_abstraction import (
     EffectsAbstraction,
 )
-from analysis.smt_checker import sequence_interpolant
+from analysis.smt_checker import quantifier_elimination, sequence_interpolant
 from parsing.string_to_prop_logic import string_to_prop
 from programs.program import Program
 from programs.util import reduce_up_to_iff
 from prop_lang.biop import BiOp
-from prop_lang.types.ops_and_rels import MathRels
-from prop_lang.types.types import BOOLEAN
+from prop_lang.types.types import typed_var_to_pysmt_type
+from prop_lang.types.values import BoolAtoms
 from prop_lang.util import (
     neg,
     conjunct_formula_set,
@@ -19,6 +21,9 @@ from prop_lang.util import (
     is_contradictory,
     atomic_predicates,
     normalise_pred_multiple_vars,
+    sat,
+    conjunct,
+    simplify_formula_with_math,
 )
 from prop_lang.value import Value
 from prop_lang.variable import Variable
@@ -56,16 +61,21 @@ def safety_refinement_seq_int(
         for i, (tran, prog_state, cs_state) in enumerate(agreed_on_transitions):
             if i == 0:
                 init_formula = [
-                    BiOp(Variable(v), MathRels.EQ, val)
-                    for v, val in program.init_var_values.items()
+                    BiOp(e, "=", Value(prog_state[str(e)])) for e in program.local_vars
                 ]
-                p_0 = conjunct_formula_set(init_formula).replace_vars(ith_vars(0))
-                act = [
-                    u.replace_vars(
-                        {e: Value(cs_state[str(e)]) for e in program.num_in_out}
-                    )
-                    for u in tran.action
-                ]
+                ps = []
+                for k, v in cs_state.items():
+                    if k.startswith("pred_"):
+                        pred = var_to_predicate(k)
+                        if not add_actions and "_prev" in str(pred):
+                            continue
+                        if v == "TRUE":
+                            ps.append(pred)
+                        else:
+                            ps.append(neg(pred))
+                p_0 = conjunct_formula_set(ps + init_formula).replace_vars(ith_vars(0))
+
+                act = tran.action
                 us_0 = [
                     BiOp(
                         Variable(str(u.left) + "_1"),
@@ -102,12 +112,15 @@ def safety_refinement_seq_int(
                     }
                 )
                 g_i = g.replace_vars(ith_vars(i))
-                act = [
-                    u.replace_vars(
-                        {e: Value(cs_state[str(e)]) for e in program.inp_out_puts}
-                    )
-                    for u in tran.action
+                act = tran.action + [
+                    BiOp(e, "=", Value(cs_state[str(e)])) for e in program.num_in_out
                 ]
+                #     [
+                #     u.replace_vars(
+                #         {e: Value(cs_state[str(e)]) for e in program.inp_out_puts}
+                #     )
+                #     for u in tran.action
+                # ]
                 us_i = [
                     BiOp(
                         Variable(str(u.left) + "_" + str(i + 1)),
@@ -135,104 +148,56 @@ def safety_refinement_seq_int(
 
         new_state_preds_fnode = sequence_interpolant(formulas_fnode)
 
-        if new_state_preds_fnode is None:
-            raise Exception(
-                "Bug: Counterexample does not contain a sequence interpolant."
-            )
         reset_vars = {
             Variable(v + "_" + str(i)): Variable(v)
             for v in program.symbol_table.keys()
             for i in range(0, len(agreed_on_transitions) + 1)
         }
 
-        new_state_preds = [
-            fnode_to_formula(f).replace_vars(reset_vars) for f in new_state_preds_fnode
-        ]
-        new_state_preds = [p for ps in new_state_preds for p in atomic_predicates(ps)]
-        new_state_preds = list(set(new_state_preds))
-        new_state_preds = [
-            x
-            for x in new_state_preds
-            if not is_tautology(x, symbol_table)
-            and not is_contradictory(x, symbol_table)
-        ]
+        old_state_predicates = predicate_abstraction.raw_state_predicates
 
-    state_predicates = set()
-    state_predicates.update(
-        s.pred for s in predicate_abstraction.get_state_predicates()
-    )
-    state_predicates.update(
-        s for c in predicate_abstraction.v_to_chain_pred.values() for s in c.chain
-    )
-    # new_state_preds = list(itertools.chain.from_iterable(map(lambda x: x[1], map(normalise_predicate, new_state_preds))))
-    normalised_state_preds = set()
-    for p in new_state_preds:
-        result = normalise_pred_multiple_vars(p, signatures, program.symbol_table)
-        if len(result) == 1:
-            normalised_state_preds.add(result)
+        success = True
+
+        if new_state_preds_fnode:
+            new_state_preds = [
+                fnode_to_formula(f).replace_vars(reset_vars)
+                for f in new_state_preds_fnode
+            ]
+            new_state_preds = [
+                p for ps in new_state_preds for p in atomic_predicates(ps)
+            ]
+
+            new_state_preds = normalise_and_filter_preds(
+                old_state_predicates, new_state_preds, signatures, symbol_table
+            )
+
+            success = not predicates_known(
+                old_state_predicates, new_state_preds, symbol_table
+            )
         else:
-            sig, _, preds = result
-            signatures.add(sig)
-            normalised_state_preds.update(preds)
-    new_state_preds = normalised_state_preds
-    new_all_preds = new_state_preds | state_predicates
+            success = False
 
-    new_all_preds = reduce_up_to_iff(
-        state_predicates,
-        list(new_all_preds),
-        symbol_table,
-    )
+        if not success:
+            new_state_preds = qe_refinement(
+                formulas,
+                old_state_predicates,
+                reset_vars,
+                predicate_abstraction.program.symbol_table,
+                new_symbol_table,
+            )
+            new_state_preds = normalise_and_filter_preds(
+                old_state_predicates, new_state_preds, signatures, symbol_table
+            )
 
-    if len(new_all_preds) < len(set(state_predicates)):
-        raise Exception("There are somehow less state predicates than previously.")
+            if len(new_state_preds) == 0:
+                raise Exception("No new state predicates identified.")
 
-    if len(set(new_all_preds)) == len(set(state_predicates)):
-        if len(program.num_in_out) == 0:
-            raise Exception("Did not find new state predicates.")
-
-        new_state_preds = set()
-        ts = [(t, cs_state) for t, _, cs_state in agreed_on_transitions]
-        for t, cs_state in ts:
-            for act in t.action:
-                for v in act.right.variablesin():
-                    to_replace = {}
-                    if v in program.num_in_out and symbol_table[v.name] != BOOLEAN:
-                        pred = BiOp(v, "=", Value(cs_state[str(v)]))
-                        to_replace[pred.left] = pred.right
-                        sig, _, preds = normalise_pred_multiple_vars(
-                            pred, signatures, symbol_table
-                        )
-                        new_state_preds.update(preds)
-                        signatures.add(sig)
-
-                    pred = BiOp(act.left, "=", act.right.replace(to_replace))
-                    if symbol_table[act.left.name] != BOOLEAN:
-                        sig, _, preds = normalise_pred_multiple_vars(
-                            pred, signatures, symbol_table
-                        )
-                        new_state_preds.update(preds)
-                        signatures.add(sig)
-
-        # cs_states = [cs_state for _, _, cs_state in agreed_on_transitions] + [disagreed_on_state[1][2]]
-        # for cs_state in cs_states:
-        #     for v in program.num_in_out:
-        #         pred = BiOp(v, "=", Value(cs_state[str(v)]))
-        #         sig, _, preds = normalise_pred_multiple_vars(pred, signatures, symbol_table)
-        #         new_state_preds.update(preds)
-        #         signatures.add(sig)
-        new_all_preds = new_state_preds | state_predicates
+        signatures.add(sig)
+        new_all_preds = new_state_preds | old_state_predicates
         new_all_preds = reduce_up_to_iff(
-            state_predicates,
+            old_state_predicates,
             new_all_preds,
-            symbol_table
-            | {
-                str(v): TypedValuation(
-                    str(v), symbol_table[str(v).removesuffix("_prev")].type, "true"
-                )
-                for p in new_all_preds
-                for v in p.variablesin()
-                if str(v).endswith("prev")
-            },
+            symbol_table,
         )  # TODO symbol_table needs to be updated with prevs
 
         # check_for_nondeterminism_last_step(program_actually_took[1], predicate_abstraction.py.program, True)
@@ -244,7 +209,7 @@ def safety_refinement_seq_int(
             [
                 str(p)
                 for p in new_all_preds
-                if p not in state_predicates and neg(p) not in state_predicates
+                if p not in old_state_predicates and neg(p) not in old_state_predicates
             ]
         )
     )
@@ -252,7 +217,7 @@ def safety_refinement_seq_int(
     new_preds = {
         p
         for p in new_all_preds
-        if p not in state_predicates and neg(p) not in state_predicates
+        if p not in old_state_predicates and neg(p) not in old_state_predicates
     }
     if len(new_preds) == 0:
         print("No new state predicates identified.")
@@ -274,3 +239,96 @@ def interactive_state_predicates():
         except Exception as e:
             pass
     return new_preds
+
+
+def qe_refinement(
+    formulas, state_predicates, reset_vars, symbol_table, new_symbol_table
+):
+    new_state_preds = set()
+    for i in range(0, len(formulas)):
+        typed_vars = list(
+            {
+                typed_var_to_pysmt_type(str(v), new_symbol_table[str(v)])[0]
+                for f in formulas
+                for v in f.variablesin()
+                if str(v).split("_")[-1] != str(i)
+            }
+        )
+        left = conjunct_formula_set(formulas[0:-1])
+        right = formulas[-1]
+        LHS = And(*left.to_smt(new_symbol_table))
+        RHS = And(*right.to_smt(new_symbol_table))
+        formula = ForAll(typed_vars, Implies(LHS, RHS))
+        qe = quantifier_elimination(formula)
+        pos_f = fnode_to_formula(qe)
+        preds_in_res = atomic_predicates(pos_f)
+
+        RHS = And(*neg(right).to_smt(new_symbol_table))
+        formula = ForAll(typed_vars, Implies(LHS, RHS))
+        qe = quantifier_elimination(formula)
+        neg_f = fnode_to_formula(qe)
+        preds_in_res.update(atomic_predicates(neg_f))
+
+        to_proj = {}
+        for p in preds_in_res:
+            if not sat(conjunct(p, left), new_symbol_table):
+                to_proj[p] = Value(BoolAtoms.FALSE)
+            # elif is_tautology(implies(left, p), new_symbol_table):
+            #     to_proj[p] = Value(BoolAtoms.TRUE)
+        new_pos_f = pos_f.replace_formulas(to_proj)
+        new_pos_f = simplify_formula_with_math(new_pos_f, new_symbol_table)
+
+        new_neg_f = neg_f.replace_formulas(to_proj)
+        new_neg_f = simplify_formula_with_math(new_neg_f, new_symbol_table)
+
+        new_state_preds.update(
+            [p.replace_vars(reset_vars) for p in atomic_predicates(new_pos_f)]
+        )
+        new_state_preds.update(
+            [p.replace_vars(reset_vars) for p in atomic_predicates(new_neg_f)]
+        )
+
+        # TODO these may not be the highest quality predicates
+        #       e.g., a predicate state_var < input is probably better than input < 0
+        if not predicates_known(
+            state_predicates,
+            new_state_preds,
+            symbol_table,
+        ):
+            break
+    return new_state_preds
+
+
+def predicates_known(old_state_predicates, new_state_preds, symbol_table):
+    new_all_preds = new_state_preds | old_state_predicates
+
+    new_all_preds = reduce_up_to_iff(
+        old_state_predicates,
+        list(new_all_preds),
+        symbol_table,
+    )
+
+    return len(new_all_preds) == len(set(old_state_predicates))
+
+
+def normalise_and_filter_preds(
+    old_state_predicates, new_state_preds, signatures, symbol_table
+):
+    normalised_state_preds = set()
+    for p in new_state_preds:
+        result = normalise_pred_multiple_vars(p, signatures, symbol_table)
+        if isinstance(result, Variable):
+            normalised_state_preds.add(result)
+        else:
+            sig, _, preds = result
+            signatures.add(sig)
+            normalised_state_preds.update(preds)
+
+    fresh_state_preds = {
+        x
+        for x in normalised_state_preds
+        if x not in old_state_predicates
+        and not is_tautology(x, symbol_table)
+        and not is_contradictory(x, symbol_table)
+    }
+    return fresh_state_preds
