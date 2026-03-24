9380496f5ef4afb31c547f952c75819c839656c4
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2026-03-24 14:58:10 +0100

    fix: corrected realisablity logging for dual2

--- src/analysis/refinement/refinement.py
+++ src/analysis/refinement/refinement.py
@@ -44,8 +44,9 @@ def refinement_standard(
         + str(time.time() - start)
     )
     dual = config.Config.getConfig().dual
+    dual2 = config.Config.getConfig().dual2
     if determination:
-        if dual:
+        if dual or dual2:
             logging.info("Problem confirmed realisable.")
         else:
             logging.info("Problem confirmed unrealisable.")
