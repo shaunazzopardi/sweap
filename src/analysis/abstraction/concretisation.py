ca9afc43f3b2161e3539649407e217d23e9abf33
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2026-04-10 00:06:40 +0200

    chore: cleanup

--- src/analysis/abstraction/concretisation.py
+++ src/analysis/abstraction/concretisation.py
@@ -68,13 +68,9 @@ def concretize_transitions(program, indices_and_state_list, incompatible_state):
             else:
                 concretized += [(stutter_trans, program_state, cs_state)]
 
-    # two options, either we stopped because of a predicate mismatch, or a transition mismatch
+    # two options, either we stopped because of a state mismatch or a predicate mismatch
     incompatibility_formula = []
-    if (
-        incompatible_state[2]["compatible_states"]
-        == "FALSE"
-        # or incompatible_state[2]["compatible_outputs"] == "FALSE"
-    ):
+    if incompatible_state[2]["compatible_states"] == "FALSE":
         if program.deterministic is None:
             if not is_deterministic(program):
                 raise Exception(
@@ -241,79 +237,4 @@ def process_transition_mismatch(program, concretized, incompatible_state):
     else:
         raise Exception(
             "Program is non-deterministic, cannot handle refinement for it."
-        )
-        # if program is not deterministic, we need to identify the transitions the counterstrategy wanted to take rather than the one the program actually took
-        try:
-            state_before_mismatch = concretized[-2][1]
-        except Exception as e:
-            raise e
-        src_state = concretized[-2][0].tgt
-        tgt_state_env_wanted = [
-            p for p in program.states if incompatible_state["mon_" + str(p)] == "TRUE"
-        ][0]
-        outputs_env_wanted = [
-            p
-            for p in program.out_events
-            if incompatible_state["mon_" + str(p)] == "TRUE"
-        ]
-        outputs_env_wanted += [
-            neg(p)
-            for p in program.out_events
-            if incompatible_state["mon_" + str(p)] == "FALSE"
-        ]
-        if incompatible_state["turn"] == "mon_env":
-            candidate_transitions = [
-                t
-                for t in program.transitions
-                if t.src == src_state
-                and t.tgt == tgt_state_env_wanted
-                and set(t.output) == set(outputs_env_wanted)
-            ]
-            if tgt_state_env_wanted == src_state:
-                stutter = stutter_transition(program, src_state, True)
-                if stutter is not None:
-                    candidate_transitions.append(stutter)
-        elif incompatible_state["turn"] == "mon_con":
-            candidate_transitions = [
-                t
-                for t in program.con_transitions
-                if t.src == src_state
-                and t.tgt == tgt_state_env_wanted
-                and set(t.output) == set(outputs_env_wanted)
-            ]
-            if tgt_state_env_wanted == src_state:
-                stutter = stutter_transition(program, src_state, False)
-                if stutter is not None:
-                    candidate_transitions.append(stutter)
-        else:
-            raise Exception("Mismatches should only occur at mon_env or mon_con turns")
-
-        compatible_with_abstract_state = []
-        for str_p in state_before_mismatch.keys():
-            if is_predicate_var(str_p):
-                p = Variable(str_p)
-                if state_before_mismatch[str_p] == "TRUE":
-                    compatible_with_abstract_state += [var_to_predicate(p)]
-                else:
-                    compatible_with_abstract_state += [neg(var_to_predicate(p))]
-
-        abstract_state = conjunct_formula_set(compatible_with_abstract_state)
-        env_desired_transitions = [
-            t
-            for t in candidate_transitions
-            if check(
-                And(
-                    *abstract_state.to_smt(program.symbol_table),
-                    *t.condition.to_smt(program.symbol_table)
-                )
-            )
-        ]
-        formula = disjunct_formula_set(
-            [t.condition for t in env_desired_transitions]
-            + [propagate_negations(neg(concretized[-1][0].condition))]
-        )
-        return (
-            concretized[:-1],
-            ([formula], concretized[-1][1]),
-            concretized[-1],
         )
